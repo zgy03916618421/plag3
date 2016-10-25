80d09007e31ab8660d15a2fc662d9ca3820ce5d1
zgy03916618421 <576428698@qq.com>
2016-10-25 14:30:54 +0800

    reset

--- router/router.js
+++ router/router.js
@@ -5,24 +5,24 @@ var parse = require('co-busboy');
 var router = require('koa-router')();
 var C = require('../controller/controller');
 var fs = require('fs');
-router.post('/dev/suriv/login',C.login);
-router.get('/dev/suriv/oauth',C.oauth);
-router.post('/dev/suriv/uploadpic',C.upPic);
-router.post('/dev/suriv/virus',C.createVirus);
-router.get('/dev/suriv/virus/:userid',C.fightVirus);
-router.put('/dev/suriv/favor',C.favor);
-router.put('/dev/suriv/disfavor',C.disfavor);
-router.post('/dev/suriv/speed',C.speed);
-router.get('/dev/suriv/share/:userid',C.share);
-router.post('/dev/suriv/recharge',C.recharge);
-router.get('/dev/suriv/getuser/:userid',C.getUserInfo)
-router.get('/dev/suriv/path/:userid/:vid',C.path);
-router.get('/dev/suriv/tree/:vid',C.tree);
-router.get('/dev/suriv/getshare/:carryid/:vid/:userid',C.shareVirus);
-router.get('/dev/suriv/graph/:vid',C.graph);
-router.get('/dev/suriv/hotvirus',C.hotvirus);
+router.post('/login',C.login);
+router.get('/oauth',C.oauth);
+router.post('/uploadpic',C.upPic);
+router.post('/virus',C.createVirus);
+router.get('/virus/:userid',C.fightVirus);
+router.put('/favor',C.favor);
+router.put('/disfavor',C.disfavor);
+router.post('/speed',C.speed);
+router.get('/share/:userid',C.share);
+router.post('/recharge',C.recharge);
+router.get('/getuser/:userid',C.getUserInfo)
+router.get('/path/:userid/:vid',C.path);
+router.get('/tree/:vid',C.tree);
+router.get('/getshare/:carryid/:vid/:userid',C.shareVirus);
+router.get('/graph/:vid',C.graph);
+router.get('/hotvirus',C.hotvirus);
 /*router.get('/virusdetail/:vid',C.v);*/
-router.get('/dev/suriv/myviruslist',C.myViruslist);
-router.get('/dev/suriv/myspeedlist',C.mySpeedlist);
-router.post('/dev/suriv/speedcomment',C.speedComment);
+router.get('/myviruslist',C.myViruslist);
+router.get('/myspeedlist',C.mySpeedlist);
+router.post('/speedcomment',C.speedComment);
 module.exports = router
