3f6745541907ccdd30a5c83e21995b15ba6bbb1f
zgy03916618421 <576428698@qq.com>
2016-10-31 18:47:09 +0800

    appid

--- config/pingconfig.js
+++ config/pingconfig.js
@@ -3,4 +3,4 @@
  */
 exports.testKey = 'sk_test_iLaT08fPSOGOWzn54Oqr5uX1';
 exports.liveKey = 'sk_live_PC0m1GrXLKe1DiTeP8G4m5q1';
-exports.appID = 'app_z54en5fPGqj5mrP0';
+exports.appID = 'app_40GCqT4K4yv90mD0';
