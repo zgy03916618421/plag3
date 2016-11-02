fb3db296e0a62ea8b27b9dfd717685d0b9622567
zgy03916618421 <576428698@qq.com>
2016-11-02 16:50:52 +0800

    kafka4

--- middleware/connectKafka.js
+++ middleware/connectKafka.js
@@ -2,10 +2,9 @@
  * Created by Administrator on 2016/11/2.
  */
 var kafka = require('kafka-node'),
-    HighLevelProducer = kafka.HighLevelProducer,
-    client = new kafka.Client('192.168.100.2:9092','kafka-node-client'),
-    producer = new HighLevelProducer(client);
-global.producer = producer;
+    Producer = kafka.Producer,
+    client = new kafka.Client('192.168.100.2:9092'),
+    producer = new Producer(client)
 var msg = {"userId":"7363ebce6fbf31d59559810a23cce220","type":"text","content":"瑞珊你好","vid":"12ca067c2f1a1f75b21494bbec54984d","timeCreated":1477641462000};
 var payloads = [
     {topic:'content',messages:JSON.stringify(msg)}
