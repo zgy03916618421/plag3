b123287435a87aadffc33c6475d7d6c6e84e469a
zgy03916618421 <576428698@qq.com>
2016-11-10 12:12:48 +0800

    jssdk5

--- controller/controller.js
+++ controller/controller.js
@@ -335,7 +335,7 @@ exports.getJssdk = function *() {
         yield redisTemplate.set(key,data);
         yield redisTemplate.expire(key,6500);
     }
-    this.body = data;
+    this.body = JSON.parse(data);
 }
 function *virus_before_ts(ts) {
     var count = yield mongodb.collection('virus').count({createtime: {$lt: ts}})
